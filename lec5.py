6f046482d6697c0daf044d96e236e4cc5602448c
EC2 Default User <ec2-user@ip-172-31-88-22.ec2.internal>
2022-02-28 23:13:23 +0000

    lab4

---
+++ lec5.py
@@ -0,0 +1,33 @@
+'''
+lec 5- if statements
+'''
+
+import this
+print (    1+
+        2)
+        
+print([1,2,3,
+    
+    4, 5, 6])
+m= 1+\
+    2
+    
+print(m)
+
+a = [1, 2, 3]
+b = [1, 2, 3]
+
+print(id([1, 2, 3]))
+print(id(a))
+print(id(b))
+
+print (a==b)
+
+x = None
+
+print (x == None)
+print(x is None) 
+
+y = []
+
+print 
