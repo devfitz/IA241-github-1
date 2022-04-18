55e7e7472d724dd29bc46e2726267fbbbc3932db
EC2 Default User <ec2-user@ip-172-31-88-22.ec2.internal>
2022-04-18 18:14:45 +0000

    lab seven.

---
+++ lec6.py
@@ -0,0 +1,14 @@
+'''
+lec 6- for loops
+''' 
+
+for i in ["a", "b", "c"]:
+    print(i)
+    
+demo_str = "this is my string"
+
+for c in demo_str:
+    print(c)
+    
+for word in demo_str.split():
+    print(word)
